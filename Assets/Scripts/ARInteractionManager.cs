6340ed05436688798dbad5bfdf6e61a1f27aad9c
Ricardo Fernández <ricfdzgon@gmail.com>
2022-06-09 18:12:34 +0200

    Pruebas Varias
    Botón borrar Debug
    Instanciado de sphere
    Colocación Planta

--- Assets/Scripts/ARInteractionManager.cs
+++ Assets/Scripts/ARInteractionManager.cs
@@ -22,27 +22,52 @@ public class ARInteractionManager : MonoBehaviour
         set
         {
             currentObject = value;
-            currentObject.transform.position = pointer.transform.position;
-            currentObject.transform.parent = pointer.transform;
+            currentObject.transform.position = transform.position;
+            currentObject.transform.parent = transform;
             isInitialPosition = true;
         }
     }
-    void Start()
-    {
-
-    }
-
     void Update()
     {
-        Vector2 middleScreen = new Vector2(Screen.width / 2, Screen.height / 2);
-        raycastManager.Raycast(middleScreen, hits, TrackableType.FeaturePoint);
-        debugText.text = hits.Count.ToString();
+        if (isInitialPosition)
+        {
+            Vector2 middleScreen = new Vector2(Screen.width / 2, Screen.height / 2);
+            raycastManager.Raycast(middleScreen, hits, TrackableType.FeaturePoint);
 
-        //Ray ray = Camera.main.ScreenPointToRay(middleScreen);
+            if (hits.Count > 0)
+            {
+                //Colocamos o obxecto
+                transform.position = hits[0].pose.position;
+                debugText.text = debugText.text + " | " + "Punto 1";
+                transform.rotation = hits[0].pose.rotation;
+                debugText.text = debugText.text + " | " + "Punto 2";
+                pointer.SetActive(true);
+                debugText.text = debugText.text + " | " + "Punto 3";
+
+                isInitialPosition = false;
+
+                debugText.text = debugText.text + " | " + "Colocamos o obxecto";
+            }
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touchOne = Input.GetTouch(0);
+            if (touchOne.phase == TouchPhase.Began)
+            {
+                debugText.text = debugText.text + " | " + "Toque en " + touchOne.position;
+            }
+        }
     }
 
     public void GreenButtonOnClick()
     {
         debugText.text = debugText.text + " | Green Button Clicked";
+        CurrentObject = Instantiate(objectPrefab);
+    }
+
+    public void BorrarDebugButtonOnClick()
+    {
+        debugText.text = "";
     }
 }
